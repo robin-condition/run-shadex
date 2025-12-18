30992599481b3b2102b49257d31ea7e4b4475506
Robin Husman <robinlhusman@gmail.com>
2025-12-17 19:33:44 -0500

    Reorganized into workspace and added a spot for gui crate.

--- shadex-backend/src/main.rs
+++ shadex-backend/src/main.rs
@@ -1,13 +1,13 @@
-use run_shadex::{
+use shadex_backend::{
     execution::typechecking,
     parsing::{construct_node_graph, parse_whole_input, type_parsing::parse_type_universe},
 };
 
 fn main() {
-    let types_text = include_str!("../examples/typeland.shadextypes");
+    let types_text = include_str!("../../examples/typeland.shadextypes");
     let universe = parse_type_universe(types_text).unwrap();
 
-    let input_text = include_str!("../examples/test_fv.shadex");
+    let input_text = include_str!("../../examples/test_fv.shadex");
     let full_output = parse_whole_input(input_text.as_bytes()).unwrap().1;
     let constructed = construct_node_graph(universe, full_output).unwrap();
 
---
+++ shadex-gui/src/main.rs
@@ -0,0 +1,18 @@
+// https://github.com/emilk/eframe_template/tree/main
+
+
+fn main() -> eframe::Result {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([400.0, 300.0])
+            .with_min_inner_size([300.0, 220.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "eframe template",
+        native_options,
+        Box::new(|cc| Ok(Box::new(shadex_gui::App::new(cc)))),
+    )
+}
