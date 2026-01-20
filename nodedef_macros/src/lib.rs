a2b4d021f4923da294cbcd3179c3e5ee8518dbb1
Robin Husman <robinlhusman@gmail.com>
2026-01-20 17:45:20 -0500

    Made a 'runtime-parse' macro.

--- nodedef_macros/src/lib.rs
+++ nodedef_macros/src/lib.rs
@@ -12,6 +12,26 @@ pub fn nodedef(input: TokenStream) -> TokenStream {
     todo!()
 }
 
+#[proc_macro]
+pub fn include_nodedef(input: TokenStream) -> TokenStream {
+    todo!()
+}
+
+#[proc_macro]
+pub fn runtime_parse(input: TokenStream) -> TokenStream {
+    let inp = parse_macro_input!(input as LitStr);
+    let inp_str = inp.value();
+
+    let expanded = quote::quote! {
+    {let mut parse = shadex_computation_definitions::nodedef::parsing::parse_expr().whole_file();
+    let expr = parse.parse_complete(#inp_str).unwrap().1;
+    expr
+    }
+    };
+
+    TokenStream::from(expanded)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
--- shadex-gui/src/main.rs
+++ shadex-gui/src/main.rs
@@ -2,8 +2,6 @@
 
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result {
-    nodedef_macros::nodedef!("hi");
-
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
     let native_options = eframe::NativeOptions {
